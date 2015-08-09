878a2ab895533e5c6f057845385cbedfbe552144
Isaac Saqib <isaacsaqib1@Isaacs-MBP.home>
2015-08-09 06:38:07 -0400

    adding images in

---
+++ app/assets/javascripts/new.js
@@ -0,0 +1,16 @@
+ $("body > table > tbody > tr:nth-child(3) > td:nth-child(2") > form > button).on("click", function(){
+          console.log("hello");
+        })
+        // $.ajax ({
+        //     url: "/orders",
+        //     type: "POST",
+        //     data: { order.address },
+        //     dataType: "JSON"
+        // }).done(function(data){
+
+
+$("#top-pic").on("click", function(){
+	$(this).css("opacity", .3);
+
+
+})
