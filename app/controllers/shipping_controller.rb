57abab9360085c151866f2486cb79c153480f12c
Isaac Saqib <isaacsaqib1@Isaacs-MBP.home>
2015-08-24 06:34:04 -0400

    fixing tax issue

--- app/controllers/shipping_controller.rb
+++ app/controllers/shipping_controller.rb
@@ -6,20 +6,16 @@ if params[:firstname]
  	session[:lastname] = params[:lastname]
  	session[:email] = params[:email]
  	session[:address] = params[:address]
- 	#session[:state] = params[:city]
+ 	session[:city] = params[:city]
  	session[:state] = params[:state]
  	session[:zip] = params[:zip]
  	session[:country] = params[:country]
  	session[:shipping] = params[:shipping]
-
- 	if params[:state]=='NY' && params[:select_state]!='Select state'
- 		session[:state] = params[:select_state]
- 		session[:tax] = 0.0875
+ 	session[:tax] = 'null'
+ 	if params[:select_state]=='NY'
+ 		session[:tax] = 0.087
  	end
- 	if params[:state]!='' && params[:select_state]=='Select state'
- 		session[:state] = params[:state]
- 		session[:tax] = 'null'
- 	end
+ 
  	
  	redirect_to "/checkout"
  end
