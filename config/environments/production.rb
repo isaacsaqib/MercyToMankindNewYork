07b69260572a11cba221e0a5ea3cb83cdf92eaef
Isaac Saqib <isaac.saqib@stonybrook.edu>
2015-10-28 06:58:14 -0400

    sec

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,16 +1,16 @@
 Rails.application.configure do
 
   
-  Paperclip::Attachment.default_options.merge!({
-    storage: :s3,
-    s3_credentials: {
-        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-        bucket: "#{ENV['AWS_BUCKET']}-#{Rails.env}"
-        },
-    url: ":s3_domain_url",
-    path: "/:class/:attachment/:id_partition/:style/:filename"
-    })
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
 
 ENV['PUBLISHABLE_KEY']='pk_test_85dgBVCarVWNxR6rCOEovf2p'
 ENV['SECRET_KEY']='sk_test_CtY58gVmXdha8d24wKMoRZCI'
