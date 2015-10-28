58db765ccf3afd166760969be5fa05ac724b3c4b
Isaac Saqib <isaac.saqib@stonybrook.edu>
2015-10-27 22:31:01 -0400

    bucket

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
     s3_credentials: {
         access_key_id: ENV['AWS_ACCESS_KEY_ID'],
         secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-        bucket: ENV['AWS_BUCKET']
+        bucket: "#{ENV['AWS_BUCKET']}-#{Rails.env}"
         },
     url: ":s3_domain_url",
     path: "/:class/:attachment/:id_partition/:style/:filename"
