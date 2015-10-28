8a00347fa83da126fa9cc62c59d5ef25f1a7efd2
Isaac Saqib <isaac.saqib@stonybrook.edu>
2015-10-27 21:40:25 -0400

    testing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,13 @@
 class Listing < ActiveRecord::Base
-	# has_attached_file :pictures, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :pictures,
+	:storage => :s3,
+  	:s3_credentials => {
+    :bucket => ENV['AZ_BUCKET'],
+    :access_key_id => ENV['AZ_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AZ_SECRET_ACCESS_KEY']
+  }
+
   	# validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 	has_many :pictures, :dependent => :destroy
+
 end
