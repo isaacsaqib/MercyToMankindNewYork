8c2f21f3de9b2cc203ea31ea3a1429ab8e3fc3d2
Isaac Saqib <isaac.saqib@stonybrook.edu>
2015-10-28 07:31:05 -0400

    sec

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,10 +4,8 @@ class Picture < ActiveRecord::Base
     s3_credentials: {
     	access_key_id: ENV['AWS_ACCESS_KEY_ID'],
         secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-        bucket: "#{ENV['AWS_BUCKET']}-#{Rails.env}",
-        },
-    url: ":s3_domain_url",
-    path: "/:class/:attachment/:id_partition/:style/:filename"
+        bucket: "#{ENV['AWS_BUCKET']}-#{Rails.env}"
+        }
     })
 
   belongs_to :listing
