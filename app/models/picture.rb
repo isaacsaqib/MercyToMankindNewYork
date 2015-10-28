4d54454f84230eb2fe18747cf5b41e5755a0f563
Isaac Saqib <isaac.saqib@stonybrook.edu>
2015-10-28 07:18:32 -0400

    sec

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,12 +1,12 @@
 class Picture < ActiveRecord::Base
+
  has_attached_file :image,
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
+                      :storage        => :s3,
+                      :s3_credentials => {:bucket            => ENV['AWS_BUCKET'],
+                                          :access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+                                          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']},
+                      :s3_protocol    => "https"                                             ,
+                      :s3_host_name   => "s3-eu-west-1.amazonaws.com"  
 
   belongs_to :listing
 
