1d319deefc3673e832deeb1268861222072a864b
Isaac Saqib <isaac.saqib@stonybrook.edu>
2015-11-01 16:16:38 -0500

    changing font

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,12 +43,14 @@ ENV['SECRET_KEY']='sk_live_Zo6Tcaeyh7EADgohnxy58TqE'
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
-
+  
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
+  
+config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
