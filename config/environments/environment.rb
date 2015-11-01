249ca1b63166b152f1bc988f1c992fc3358f2af8
Isaac Saqib <isaac.saqib@stonybrook.edu>
2015-11-01 06:19:13 -0500

    trsting

--- config/environments/environment.rb
+++ config/environments/environment.rb
@@ -3,13 +3,13 @@ ENV['AWS_BUCKET']='mxmny'
 ENV['AWS_ACCESS_KEY_ID'] = "AKIAJZG2MFC64VXKUDEQ"
 ENV['AWS_SECRET_ACCESS_KEY'] = "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"
 
-
-# ENV['PUBLISHABLE_KEY']='pk_live_qQtQHJtmcR1SI16gQlcAStly'
-# ENV['SECRET_KEY']='sk_live_Zo6Tcaeyh7EADgohnxy58TqE'
+# live
+ENV['PUBLISHABLE_KEY']='pk_live_qQtQHJtmcR1SI16gQlcAStly'
+ENV['SECRET_KEY']='sk_live_Zo6Tcaeyh7EADgohnxy58TqE'
 
 
 # Test 
-ENV['PUBLISHABLE_KEY']='pk_test_85dgBVCarVWNxR6rCOEovf2p'
-ENV['SECRET_KEY']='sk_test_CtY58gVmXdha8d24wKMoRZCI'
+# ENV['PUBLISHABLE_KEY']='pk_test_85dgBVCarVWNxR6rCOEovf2p'
+# ENV['SECRET_KEY']='sk_test_CtY58gVmXdha8d24wKMoRZCI'
 
 
