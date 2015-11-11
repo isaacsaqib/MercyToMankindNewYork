5eff6ebb5dd990e6071fe001594812f4565006da
Isaac Saqib <isaac.saqib@stonybrook.edu>
2015-11-11 02:30:20 -0500

    adding in ig slide

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,8 @@
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( jquery.js )
+Rails.application.config.assets.precompile += %w( jquery.slides.js )
+Rails.application.config.assets.precompile += %w( jquery.slides.min.js )
 
 
 # Add additional assets to the asset load path
