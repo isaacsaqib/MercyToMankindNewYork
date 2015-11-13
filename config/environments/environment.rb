f6925c73615cc1fdf62191168ebd3c9afbc22757
Isaac Saqib <isaac.saqib@stonybrook.edu>
2015-11-13 03:55:54 -0500

    live keys

--- config/environments/environment.rb
+++ config/environments/environment.rb
@@ -4,12 +4,12 @@ ENV['AWS_ACCESS_KEY_ID'] = "AKIAJZG2MFC64VXKUDEQ"
 ENV['AWS_SECRET_ACCESS_KEY'] = "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"
 
 # live
-# ENV['PUBLISHABLE_KEY']='pk_live_qQtQHJtmcR1SI16gQlcAStly'
-# ENV['SECRET_KEY']='sk_live_Zo6Tcaeyh7EADgohnxy58TqE'
+ENV['PUBLISHABLE_KEY']='pk_live_qQtQHJtmcR1SI16gQlcAStly'
+ENV['SECRET_KEY']='sk_live_Zo6Tcaeyh7EADgohnxy58TqE'
 
 
 # Test 
-ENV['PUBLISHABLE_KEY']='pk_test_85dgBVCarVWNxR6rCOEovf2p'
-ENV['SECRET_KEY']='sk_test_CtY58gVmXdha8d24wKMoRZCI'
+# ENV['PUBLISHABLE_KEY']='pk_test_85dgBVCarVWNxR6rCOEovf2p'
+# ENV['SECRET_KEY']='sk_test_CtY58gVmXdha8d24wKMoRZCI'
 
 
