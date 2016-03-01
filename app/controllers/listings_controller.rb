0fcdffbfdba16b533a742613dbe09ef28ba14f37
Isaac Saqib <isaac.saqib@stonybrook.edu>
2016-03-01 15:56:50 -0500

    new

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -10,7 +10,7 @@ class ListingsController < ApplicationController
 		@listings = Listing.all
 
 		@listings_new = Listing.where(:section => "New")
-		@listings_tops= Listing.where(:section => "Tops").order(price: :desc)
+		@listings_tops= Listing.where(:section => "Tops")
 
 		@listings_bottoms = Listing.where(:section => "Bottoms").order(price: :desc)
 		@listings_headwear = Listing.where(:section => "Headwear").order(price: :desc)
