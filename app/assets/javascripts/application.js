4491641d1d6d4751d8b763ca4b9d0b948b031722
Isaac Saqib <isaac.saqib@stonybrook.edu>
2016-02-29 22:31:47 -0500

    trouble

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,14 +42,6 @@ $(document).ready(function(){
 
 
 
-	 if 
-	 	( $("img[alt*='M2mk']") )
-	 {
-	 	// console.log('hello')
-	 	$( "img[alt*='M2mk']").css("width", "40%");
-
-	 }
-
 	
 
 
---
+++ app/assets/javascripts/listing.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+	 if 
+	 	( $("img[alt*='M2mk']") )
+	 {
+	 	// console.log('hello')
+	 	$( "img[alt*='M2mk']").css("width", "40%");
+
+	 }
+}
