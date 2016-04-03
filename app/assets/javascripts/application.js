95f4f9a778086a4a58b99efa04e5e755c0f15cc0
Isaac Saqib <isaac.saqib@stonybrook.edu>
2016-04-03 02:17:20 -0400

    adjusting shipping page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -40,6 +40,8 @@ $(document).ready(function(){
 
 	})
 
+	
+
 
 
 
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,7 @@ Rails.application.config.assets.precompile += %w( jquery.slides.js )
 Rails.application.config.assets.precompile += %w( jquery.slides.min.js )
 
 
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
