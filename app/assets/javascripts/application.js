a24e93b9700ac4776995bf03a226584fd4dec562
Isaac Saqib <isaac.saqib@stonybrook.edu>
2016-05-22 05:13:14 -0400

    bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -45,29 +45,29 @@ $(document).ready(function(){
 
 
 
-//  if 
-// 	 	( $("img[alt*='M2mk']") )
-// 	 {
+ if 
+	 	( $("img[alt*='M2mk']") )
+	 {
 
-// 	 	$( "img[alt*='M2mk']").css("width", "80%");
+	 	$( "img[alt*='M2mk']").css("width", "80%");
 
-// 	 };
+	 };
 
-//  if 
-// 	 	( $("img[alt='M2mk final 16 copy']") )
-// 	 {
+ if 
+	 	( $("img[alt='M2mk final 16 copy']") )
+	 {
 
-// 	 	$( "img[alt='M2mk final 16 copy']").css("width", "100%");
+	 	$( "img[alt='M2mk final 16 copy']").css("width", "100%");
 
-// 	 };
+	 };
 
-// if 
-// 	 	( $("img[alt='M2mk final 29 copy']") )
-// 	 {
+if 
+	 	( $("img[alt='M2mk final 29 copy']") )
+	 {
 
-// 	 	$( "img[alt='M2mk final 29 copy']").css("width", "100%");
+	 	$( "img[alt='M2mk final 29 copy']").css("width", "100%");
 
-// 	 };
+	 };
 
 // if 
 // 	 	( $("img[alt='M2mk final 23 copy']") )
