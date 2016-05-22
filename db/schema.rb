69158c3f1472e6baac9694568e40a35225ac127c
Isaac Saqib <isaac.saqib@stonybrook.edu>
2016-05-22 05:55:50 -0400

    adding sessions table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301210302) do
+ActiveRecord::Schema.define(version: 20160522095512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,9 @@ ActiveRecord::Schema.define(version: 20160301210302) do
     t.datetime "updated_at"
     t.string   "listing_token"
     t.string   "image"
+  end
+
+  create_table "sessions", force: :cascade do |t|
   end
 
   create_table "todos", force: :cascade do |t|
