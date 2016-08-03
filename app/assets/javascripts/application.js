e2b3c4d27ee2030a345ec4d314b21072b7c45f3a
Isaac Saqib <isaac.saqib@stonybrook.edu>
2016-08-03 02:26:19 -0400

    testing alert

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -103,6 +103,7 @@ $(document).ready(function(){
 
 
 	$("#listings-index-nav > li:nth-child(4)").on("click", function(){ /* Headwear */
+		alert("hi")
 		$(".products").hide();
 		$("#head").fadeIn();
 		$(".headers-hide").hide();
