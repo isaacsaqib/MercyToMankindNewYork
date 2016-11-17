89da74e56e7dbed48c547b957cebfabf195bee88
Isaac Saqib <isaac.saqib@stonybrook.edu>
2016-11-17 16:24:55 -0500

    adding headwear slideshow

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,8 @@ class WelcomeController < ApplicationController
   	session[:cart] ||= {}
 	session[:cart][params[:id]] = [params[:id], params[:name], params[:desc],params[:price]]
 			@listings_slideshow = Listing.where(:section => "Slideshow")
+      @listings_head = Listing.where(:section => "Head")
+
       @pictures = @listings_slideshow[0].pictures.order(description: :asc) 
       @listing = @listings_slideshow[0]
 
