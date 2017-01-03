331ec902af33df86d1e133a677d713073df16531
IsaacSaqib <isaac.saqib@stonybrook.edu>
2017-01-03 17:55:14 -0500

    adding error page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'errors/not_found'
+
+  get 'errors/internal_server_error'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   resources :listings do
@@ -19,6 +23,9 @@ resources :users
 resources :collections
 resources :coupons
 
+
+match "/404", :to => "errors#not_found", :via => :all
+match "/500", :to => "errors#internal_server_error", :via => :all
 get "/listings-new" => "listings#new"
 get "/listings-tops" => "listings#tops"
 get "/listings-jackets" => "listings#jackets"
