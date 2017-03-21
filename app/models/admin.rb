ca3d57e5f64a82290a3ce25cc4ae51993d0d7411
IsaacSaqib <isaac.saqib@stonybrook.edu>
2017-03-21 15:52:30 -0400

    adjusting mobile site

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,5 @@
 class Admin < ActiveRecord::Base
-has_secure_password
+	has_secure_password
   validates(:username,    { :uniqueness   => { case_sensitive: false }})
   validates(:password, { :length       => { :minimum => 8, :maximum => 16 },
                          :presence     => true,
